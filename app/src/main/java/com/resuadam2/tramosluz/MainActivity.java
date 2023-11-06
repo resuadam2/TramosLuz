255aa367ec96f843aa2bb77b1149110f46cab586
resuadam2 <resuadam2@gmail.com>
2023-11-06 20:35:57 +0100

    adding next tramo and some other stuff

--- app/src/main/java/com/resuadam2/tramosluz/MainActivity.java
+++ app/src/main/java/com/resuadam2/tramosluz/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -23,68 +24,65 @@ public class MainActivity extends AppCompatActivity {
     protected static final int TRAMO3 = 19; // 19 a 23
     protected static final int COLOR_TRAMO3 = Color.RED; // 19 a 23
 
-    EditText etTime;
+    TextView tvTime;
 
     Button updateFromSystem, updateFromText;
 
-    TextView tvTramo;
+    TextView tvTramo, tvTramoNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etTime = (EditText) findViewById(R.id.etTime);
+        tvTime = (TextView) findViewById(R.id.tvTime);
         updateFromSystem = (Button) findViewById(R.id.updateFromSystem);
         updateFromText = (Button) findViewById(R.id.updateFromEditText);
         tvTramo = (TextView) findViewById(R.id.tvTramo);
-        Calendar calendar = Calendar.getInstance();
-        etTime.setText(new SimpleDateFormat("HH:mm").format(calendar.getTime()));
-        tvTramo.setText(calcularTramo(calendar));
-        tvTramo.setBackgroundColor(calcularColor(calendar));
+        tvTramoNext = (TextView) findViewById(R.id.tvTramoNext);
+
+        updateFromSystem();
 
 
         // TODO setear al edit text la hora actual y al tvTramo el color y el nombre correspondientes
 
-        updateFromText.setOnClickListener(view -> updateFromText(etTime.getText().toString()));
+        updateFromText.setOnClickListener(view -> updateFromText(tvTime.getText().toString()));
         updateFromSystem.setOnClickListener(view -> updateFromSystem());
     }
 
     /**
      * Calcula el tramo en el que se encuentra la hora pasada por parámetro
+     * actualiza el TextView del tramo, su texto y el color de fondo
      * @param instance
-     * @return el tramo en el que se encuentra la hora pasada por parámetro
      */
-    private String calcularTramo(Calendar instance) {
+    private void calcularTramo(Calendar instance) {
         int hora = instance.get(Calendar.HOUR_OF_DAY);
         if (hora >= TRAMO1 && hora < TRAMO2) {
-            return "Tramo llano";
+            tvTramo.setText("Tramo llano");
+            tvTramo.setBackgroundColor(Color.GREEN);
+            tvTramoNext.setText("Tramo valle");
+            tvTramoNext.setBackgroundColor(Color.YELLOW);
         } else if (hora >= TRAMO2 && hora < TRAMO3) {
-            return "Tramo valle";
+            tvTramo.setText("Tramo valle");
+            tvTramo.setBackgroundColor(Color.YELLOW);
+            tvTramoNext.setText("Tramo pico");
+            tvTramoNext.setBackgroundColor(Color.RED);
         } else {
-            return "Tramo punta";
-        }
-    }
-
-    /**
-     * Calcula el color en el que se encuentra la hora pasada por parámetro
-     * @param instance
-     * @return el color en el que se encuentra la hora pasada por parámetro
-     */
-    private int calcularColor(Calendar instance) {
-        int hora = instance.get(Calendar.HOUR_OF_DAY);
-        if (hora >= TRAMO1 && hora < TRAMO2) {
-            return COLOR_TRAMO1;
-        } else if (hora >= TRAMO2 && hora < TRAMO3) {
-            return COLOR_TRAMO2;
-        } else {
-            return COLOR_TRAMO3;
+            tvTramo.setText("Tramo pico");
+            tvTramo.setBackgroundColor(Color.RED);
+            tvTramoNext.setText("Tramo llano");
+            tvTramoNext.setBackgroundColor(Color.GREEN);
         }
     }
 
 
 
     private void updateFromSystem() {
+        Calendar calendar = Calendar.getInstance();
+        Toast.makeText(this, "Hora actual " +
+                new SimpleDateFormat("HH:mm").format(calendar.getTime()), Toast.LENGTH_SHORT).show();
+        tvTime.setText(new SimpleDateFormat("HH:mm").format(calendar.getTime()));
+        calcularTramo(calendar);
     }
 
     private void updateFromText(String toString) {
